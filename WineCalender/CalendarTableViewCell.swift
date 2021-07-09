c8990d8631aacf8e114010f0e3c422f42514ea81
Minju Lee <prnbljk@gmail.com>
2021-07-09 20:43:11 +0900

    캘린더 - 샘플 데이터, 일정 추가

---
+++ WineCalender/CalendarTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CalendarTableViewCell.swift
+//  WineCalender
+//
+//  Created by Minju Lee on 2021/07/09.
+//
+
+import UIKit
+
+class CalendarTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var calendarImageView: UIImageView!
+    @IBOutlet weak var calendarDateLabel: UILabel!
+    @IBOutlet weak var calendarDescriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        calendarDateLabel.textColor = .lightGray
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
