da10435a1aa1ba4b56fd76c706051dbaa71d9dbf
Kang Jae Kwon <rkdwornjs123@gmail.com>
2021-07-12 13:17:13 +0900

    SwiftyCam활용 카메라 구현

--- WineCalender/SearchViewController.swift
+++ WineCalender/SearchViewController.swift
@@ -7,34 +7,68 @@
 import UIKit
 import SwiftyCam
 
-class SearchViewController :  SwiftyCamViewController ,SwiftyCamViewControllerDelegate {
+class SearchViewController: SwiftyCamViewController, SwiftyCamViewControllerDelegate {
     
-    @IBOutlet weak var CaptureButton: UIButton!
+    @IBOutlet weak var captureButton: SwiftyCamButton!
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var flipCameraButton: UIButton!
+    var photo: UIImage?
     
-//
-//        = {
-//            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-//            button.layer.cornerRadius = 50
-//            button.layer.borderWidth = 10
-//            button.layer.borderColor = UIColor.white.cgColor
-//
-//            return button
-//        }
-//
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.gray
 
         cameraDelegate = self
+        captureButton.layer.backgroundColor = UIColor.gray.cgColor
+        captureButton.layer.borderWidth = 3
+        captureButton.layer.borderColor = UIColor.white.cgColor
+        captureButton.layer.cornerRadius = 38
+        
+        
         
        // shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
     }
     
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureButton.delegate = self
+    }
     
-    @IBAction func ChageCam(_ sender: Any) {
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
+        self.photo = photo
+        performSegue(withIdentifier: "photoSegue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? PhotoViewController {
+            vc.photo = photo
+        }
+    }
+    
+    @IBAction func toggleFlashTapped(_ sender: Any) {
+        if flashMode == .auto {
+            flashMode = .on
+            flashButton.setImage(#imageLiteral(resourceName: "flash"), for: UIControl.State())
+        }else if flashMode == .on{
+            flashMode = .off
+            flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControl.State())
+        }else if flashMode == .off{
+            flashMode = .auto
+            flashButton.setImage(#imageLiteral(resourceName: "flashauto"), for: UIControl.State())
+        }
+        
+        print("Toggle flash mode: \(flashMode)")
+    }
+    
+    @IBAction func cameraSwitchTapped(_ sender: Any) {
         switchCamera()
+        print("Camera switched to \(currentCamera)")
     }
     
 }
