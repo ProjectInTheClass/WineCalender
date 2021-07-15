a81f99e4ff2fd4f234d1ac53cfb979ffa0c5763b
Kang Jae Kwon <rkdwornjs123@gmail.com>
2021-07-15 14:53:49 +0900

    7.15 임시저장

---
+++ WineCalender/SearchController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchController.swift
+//  WineCalender
+//
+//  Created by 강재권 on 2021/07/14.
+//
+
+import Foundation
+import UIKit
+
+class SearchController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search"
+        //Do any additional setup agter loading the view
+    }
+}
