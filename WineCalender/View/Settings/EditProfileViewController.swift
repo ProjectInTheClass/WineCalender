f07f358a51a153fc27a7790069e85d6be77f0361
Minju Lee <prnbljk@gmail.com>
2021-10-21 18:37:24 +0900

    10/21 EditProfileView introduction label을 TextView로 변경 / MyWines Autolayout 수정

--- WineCalender/View/Settings/EditProfileViewController.swift
+++ WineCalender/View/Settings/EditProfileViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UITextViewDelegate {
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var nicknameTextField: UITextField!
-    @IBOutlet weak var introductionTextField: UITextField!
+    @IBOutlet weak var introductionTextView: UITextView!
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var doneButton: UIButton!
 
@@ -28,11 +28,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     }
     
     @IBAction func nicknameReturnKeyTapped(_ sender: UITextField) {
-        introductionTextField.becomeFirstResponder()
-    }
-    
-    @IBAction func introductionReturnKeyTapped(_ sender: UITextField) {
-        sender.resignFirstResponder()
+        introductionTextView.becomeFirstResponder()
     }
     
     func fetchMyProfile() {
@@ -41,7 +37,13 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
                 self.profileImageView.kf.setImage(with: user.profileImageURL, placeholder: UIImage(systemName: "person.circle.fill")!.withTintColor(.systemPurple, renderingMode: .alwaysOriginal))
                 self.emailLabel.text = user.email
                 self.nicknameTextField.text = user.nickname
-                self.introductionTextField.text = user.introduction
+                if let introduction = user.introduction {
+                    self.introductionTextView.text = introduction
+                    self.introductionTextView.textColor = UIColor(named: "blackAndWhite")
+                } else {
+                    self.introductionTextView.text = "소개"
+                    self.introductionTextView.textColor = UIColor.systemGray2
+                }
             }
         }
     }
@@ -88,6 +90,20 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         dismiss(animated: true, completion: nil)
     }
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if introductionTextView.text == "소개" {
+            introductionTextView.text = ""
+            introductionTextView.textColor = UIColor(named: "blackAndWhite")
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if introductionTextView.text == "" || introductionTextView.text == "소개" {
+            introductionTextView.text = "소개"
+            introductionTextView.textColor = UIColor.systemGray2
+        }
+    }
+    
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         guard nicknameTextField.text != "" else {
             warningLabel.text = "닉네임을 입력해 주세요."
@@ -97,7 +113,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         warningLabel.text = ""
         
         self.nicknameTextField.resignFirstResponder()
-        self.introductionTextField.resignFirstResponder()
+        self.introductionTextView.resignFirstResponder()
         
         let alert = UIAlertController(title: "프로필 수정", message: "프로필을 수정하시겠습니까?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
@@ -105,7 +121,11 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
             
             let profileImage = self.profileImageView.image!
             let nickname = self.nicknameTextField.text!
-            let introduction = self.introductionTextField.text!
+            var introduction = ""
+            if self.introductionTextView.text != "소개" {
+                introduction = self.introductionTextView.text
+            }
+            
             AuthenticationManager.shared.editUserProfile(profileImage: profileImage, nickname: nickname, introduction: introduction, warningLabel: self.warningLabel) { result in
                 if result == true,
                    let myWinesVC = self.navigationController?.children.first as? MyWinesViewController{
